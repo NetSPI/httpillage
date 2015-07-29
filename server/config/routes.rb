74e87629ff3598c8e7bb16553e5ebfa34578d711
John Poulin <john.m.poulin@gmail.com>
2015-07-29 14:33:24 -0400

    Cleaning up the routes formatting

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
 	namespace 'api' do
-	  get 'health', 			to: 'health#index'
-	  get 'poll(/:nodeid(/:jobid))', to: 'dispatcher#poll'
+	  get 'health', 									to: 'health#index'
+	  get 'poll(/:nodeid(/:jobid))', 	to: 'dispatcher#poll'
 
 	  # Intruder will use the following endpoints to create and manage jobs
-	  post 'job/create', to: 'job#create'
+	  post 'job/create', 							to: 'job#create'
 
-	  get 'test', to: 'dispatcher#test'
+	  get 'test', 										to: 'dispatcher#test'
 	end
 
-	get 'jobs', to: 'job#index'
-	get 'jobs/new', 	to: 'job#new'
-	post 'jobs', to: 'job#create'
-	get 'nodes', to: 'node#index'
-	delete 'job/:jobid', to: 'job#destroy', as: 'job'
+	get 'jobs', 									to: 'job#index'
+	get 'jobs/new', 							to: 'job#new'
+	post 'jobs', 									to: 'job#create'
+	get 'nodes', 									to: 'node#index'
+	delete 'job/:jobid', 					to: 'job#destroy', as: 'job'
+
+	get 'dictionaries',						to: 'dictionary#index'
+	get 'dictionaries/new',				to: 'dictionary#new'
+	post 'dictionaries',					to: 'dictionary#create'
 end
