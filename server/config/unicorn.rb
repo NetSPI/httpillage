022a7c79961e9308b85d0c5fa1c0f5d4fe994967
John Poulin <john.m.poulin@gmail.com>
2015-10-15 18:03:26 -0400

    Added basic unicorn config

---
+++ server/config/unicorn.rb
@@ -0,0 +1,36 @@
+
+worker_processes 2
+working_directory "/srv/httpillage/"
+ 
+# This loads the application in the master process before forking
+# worker processes
+# Read more about it here:
+# http://unicorn.bogomips.org/Unicorn/Configurator.html
+preload_app true
+ 
+timeout 30
+ 
+# This is where we specify the socket.
+# We will point the upstream Nginx module to this socket later on
+listen "/srv/httpillage/tmp/sockets/unicorn.sock", :backlog => 64
+ 
+pid "/srv/httpillage/tmp/pids/unicorn.pid"
+ 
+# Set the path of the log files inside the log folder of the testapp
+stderr_path "/srv/httpillage/log/unicorn.stderr.log"
+stdout_path "/srv/httpillage/log/unicorn.stdout.log"
+ 
+before_fork do |server, worker|
+# This option works in together with preload_app true setting
+# What is does is prevent the master process from holding
+# the database connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+ 
+after_fork do |server, worker|
+# Here we are establishing the connection after forking worker
+# processes
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
