6393091fbdb972fb156d7a1f7f1231ccc6dd2e77
John Poulin <john.m.poulin@gmail.com>
2015-11-22 18:57:10 -0500

    Fixed CSP issue

---
+++ server/app/assets/javascripts/job-edit.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#job_attack_type").change(function () {
+    var attack_type = $(this).val();
+    
+    if(attack_type == "dictionary") { 
+      $("#dictionary").show();
+      $("#charset").hide();
+    } else if(attack_type == "bruteforce") {
+      $("#charset").show();
+      $("#dictionary").hide();
+    } else {
+      $("#charset").hide();
+      $("#dictionary").hide();
+    }
+  });
+
+  var clicked = 100;
+  $("#addNewFlag").click(function () {
+    clicked++;
+    row = '<input class="input-field" type="text" name="job[response_flag_meta][' + clicked + '][match_value]" style="display:inline"><select name="job[response_flag_meta][' + clicked + '][match_type]" style="display:inline"><option value="string">String</option><option value="regex">Regex</option></select><br />';
+
+      $("#response_flags").append(row);
+      return false;
+  });
+});
