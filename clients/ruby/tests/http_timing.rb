866c84028b3720190e15895c2ee29e161ed87209
John Poulin <john.m.poulin@gmail.com>
2016-04-15 14:44:15 -0400

    Fixed tests

--- clients/ruby/tests/http_timing.rb
+++ clients/ruby/tests/http_timing.rb
@@ -1,53 +1,91 @@
 require 'mechanize'# This is a simple test to determine times for threaded HTTP requests
 require 'net/http'
 
-thread_count = 5
+@thread_count = 10
 
 # We're going to assume 1,000 requests
-request_count = 100
+@request_count = 1000
 
 # Temporary URL for testing
-target_url = "http://127.0.0.1:3000"
+@target_url = "http://52.23.237.18/wordpress/wp-login.php"
 
 @has_job = true
 
-def send_request(http_uri=nil,http_data=nil, http_headers=nil,payload=nil)
- #uri = URI('http://127.0.0.1:3000/')
- #Net::HTTP.get(uri)
- #Net::HTTP.post_form(uri, 'user[email]' => 'ruby', 'user[password]' => '50')
-begin 
+def send_net_request
+ uri = URI(@target_url)
+ Net::HTTP.post_form(uri, 'log' => 'asdf', 'pwd' => 'asdf', 'wp-submit' => 'Log+In', 'redirect_to' => 'http%3A%2F%2F52.23.237.18%2Fwordpress%2Fwp-admin%2F', 'testcookie' => '1')
+end
+
+def send_mech_request
   begin
     req = Mechanize.new
-    response = req.post("http://127.0.0.1:3000/users/sign_in", URI.decode_www_form("user[email]=ruby&user[password]=50"))
-    #response = req.get("http://127.0.0.1:3000", [], nil, nil)
+    response = req.post(@target_url, URI.decode_www_form("log=asdf&pwd=adsf&wp-submit=Log+In&redirect_to=http%3A%2F%2F52.23.237.18%2Fwordpress%2Fwp-admin%2F&testcookie=1"))
   rescue Exception => e
   end
 end
+
+def mechanize_test
+  tmp_request_count = @request_count
+  time_before = Time.now
+  puts "Starting at: #{time_before}"
+
+  job_threads = (1..@thread_count).map do |i|
+    Thread.new(i) do |i|
+      while @has_job
+        # Ideally we would check if process_request returns done
+        # If it does, let the C&C know...
+        res = send_mech_request
+        tmp_request_count -= 1
+
+        if tmp_request_count <= 0
+          @has_job = false
+        end
+      end
+    end
+  end
+
+  job_threads.each {|t| t.join }
+
+  time_after = Time.now
+  puts "Ending at: #{time_after}"
+
+  elapsed_time = (time_after - time_before).to_i
+
+  puts "Completed in #{elapsed_time} seconds"
 end
 
-time_before = Time.now
-puts "Starting at: #{time_before}"
+def net_test
+  tmp_request_count = @request_count
+  time_before = Time.now
+  puts "Starting at: #{time_before}"
 
-job_threads = (1..thread_count).map do |i|
+  job_threads = (1..@thread_count).map do |i|
   Thread.new(i) do |i|
     while @has_job
       # Ideally we would check if process_request returns done
       # If it does, let the C&C know...
-      res = send_request(target_url, nil, nil)
-      request_count -= 1
+      res = send_net_request
+      tmp_request_count -= 1
 
-      if request_count <= 0
+      if tmp_request_count <= 0
         @has_job = false
       end
     end
   end
+  end
+
+  job_threads.each {|t| t.join }
+
+  time_after = Time.now
+  puts "Ending at: #{time_after}"
+
+  elapsed_time = (time_after - time_before).to_i
+
+  puts "Completed in #{elapsed_time} seconds"
 end
 
-job_threads.each {|t| t.join }
+puts "Starting mechanize_test:"
+#mechanize_test
 
-time_after = Time.now
-puts "Ending at: #{time_after}"
-
-elapsed_time = (time_after - time_before).to_i
-
-puts "Completed in #{elapsed_time} seconds"
+puts "Starting Net::Http test:"
+net_test
